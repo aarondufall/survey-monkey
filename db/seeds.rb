768a42d0eb2447f96810fcd82b6327d4cd907afd
dbc-apprentice <apprentice@devbootcamp.com>
2013-06-14 11:25:29 -0500

    successful seed file and deleted model named option

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,13 +8,24 @@ s = Survey.new(:title => 'Fruit',
                :link => 'http://www.google.com',
                :user => rich)
 
+
+
 5.times do
   q = s.questions.build(:question_text => Faker::Lorem.sentence(5),
                         :mandatory => false,
                         :helper_text => "Answer from the heart")
     3.times do
-      q.options.build(:option_text => Faker::Lorem.sentence(3))
+      o = q.options.build(:option_text => Faker::Lorem.sentence(3))
+
+      2.times do
+        a = o.answers.build
+        s.answers << a
+      end
     end
 end
 
 s.save
+
+
+
+
