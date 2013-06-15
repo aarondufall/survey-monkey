9ad4322d1d539a7f4c4be75967c40c2918661227
Aaron Dufall <aald212@gmail.com>
2013-06-15 09:51:06 -0500

    fixed auth

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,36 +1,24 @@
 class User < ActiveRecord::Base
-
-  include BCrypt
-
-  has_many :surveys
-
-  # before_save :get_encrypted_password
-
-  validates :name, uniqueness: true, presence: true
+  attr_accessor :password_confirmation
+  before_save :encrypt_password
+  validates :name, presence: true
+  validates :email, uniqueness: true, presence: true
+  validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
   validates :password, presence: true
-  validates :email, uniqueness: true, presence: true,
-          format: { with: /.+@.+\..+/}
+  validates_confirmation_of :password
+  
 
-  # def password
-  #   @password ||= Password.new(password_hash)
-  # end
+  def self.authenticate(name, password)
+    return false if name.empty? || password.empty?
+    return false unless user = User.find_by_name(name)
+    BCrypt::Password.new(user.password) == password 
+  end
+      
 
-  # def password=(new_password)
-  #   if new_password.length > 0
-  #     @password = Password.create(new_password)
-  #     self.password_hash = @password
-  #   else
-  #     @password = ""
-  #   end
-  # end
+  private
 
-  def self.authenticate(username, password)
-    user = User.find_by_name(username)
-    if user && user.password == password 
-      user
-    else
-      false
-    end
-  end 
+  def encrypt_password
+    self.password = BCrypt::Password.create(self.password)
+  end
 
 end
